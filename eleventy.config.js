5960ac3a2c89269842a3208badbffa2f41239406
Barry Richards <barry@br-web.me>
2025-12-13 23:53:25 +0000

    Remove unnecessary configurations

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,18 +1,11 @@
-import { HtmlBasePlugin } from "@11ty/eleventy";
-
 const isProduction = process.env.ELEVENTY_ENV === "production";
 
 export default function(eleventyConfig) {
-	if(isProduction){
-		eleventyConfig.addPlugin(HtmlBasePlugin, {
-			baseHref: "https://bazzle.github.io/start-meetup/"
-		});
-	}
 	eleventyConfig.addPassthroughCopy({
 		"src/assets/scripts": "assets/scripts",
 		"src/assets/img": "assets/img"
 	});
-	const pathPrefix = isProduction ? "/start-meetup/" : "/";
+	const pathPrefix = "/";
 	return {
 		pathPrefix: pathPrefix,
 		dir: {
