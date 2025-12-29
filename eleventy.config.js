f517e2a6caeda56fbb62615ac49bc24fc2d23a7a
Barry Richards <barry@br-web.me>
2025-12-29 16:21:56 +0000

    Add download directory and add pdf

--- eleventy.config.js
+++ eleventy.config.js
@@ -4,7 +4,8 @@ export default function(eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({
 		"src/assets/scripts": "assets/scripts",
 		"src/assets/img": "assets/img",
-		"src/assets/styles/variables.css": "assets/styles/variables.css"
+		"src/assets/styles/variables.css": "assets/styles/variables.css",
+		"src/assets/downloads" : "assets/downloads"
 	});
 	const pathPrefix = "/";
 	return {
